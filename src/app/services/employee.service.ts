69128ec6eec3c1b642f78c6fab9c4e3c6e21bbaa
Neethu Sanal Kumar <117423137+Neethusanal@users.noreply.github.com>
2026-08-16 18:04:56 +0400

    urlerror

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -11,12 +11,12 @@ export class EmployeeService {
   private http = inject(HttpClient);
 
   // private apiUrl = 'http://localhost:8081/api/employees';
-  private apiUrl =' https://a46b-2001-8f8-1737-1a8b-c568-36df-b6d2-c6cd.ngrok-free.app/api/employees';
+  private apiUrl ='https://a46b-2001-8f8-1737-1a8b-c568-36df-b6d2-c6cd.ngrok-free.app/api/employees';
 
   getEmployees(): Observable<Employee[]> {
     return this.http.get<Employee[]>(this.apiUrl);
   }
-
+  
 
   createEmployee(employee: Employee): Observable<Employee> {
 
